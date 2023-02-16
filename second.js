b6fd0bd9513b97acd50696f89c1bd284997fe021
Atul Tripathi <tripathiatul0143@gmail.com>
2023-02-16 16:34:49 +0530

    deleted file

--- second.js
+++
@@ -1,7 +0,0 @@
-const backTopButton = document.getElementById('backtop')
-const topper = document.getElementById('backToTop')
-backTopButton.addEventListener('click',()=>{
-    window.scrollTo({
-        top : 0, behavior :"smooth"
-    })
-})
